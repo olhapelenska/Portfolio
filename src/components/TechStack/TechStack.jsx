a9e687d150355328987ac27ebaa3c6bf609e6e2d
Olya <pelenska.olya@gmail.com>
2025-02-13 11:24:11 -0700

    add teck stack section

--- src/components/TechStack/TechStack.jsx
+++ src/components/TechStack/TechStack.jsx
@@ -1,9 +1,11 @@
 import "./TechStack.scss";
+import StackList from "../StackList/StackList";
 
 function TechStack() {
   return (
     <section id="tech-stack" className="tech-stack">
-      <h2 className="tech-stack__title">Tech Stack</h2>
+      <h2 className="tech-stack__title">Tech Stack & Tools</h2>
+      <StackList />
     </section>
   );
 }
