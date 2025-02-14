8fa19b9f90ce0f735b9e13f0f5f2a78e8d1c9520
Olya <pelenska.olya@gmail.com>
2025-02-14 01:21:33 -0700

    refactor navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,46 +1,30 @@
 import "./Navbar.scss";
 import { Link } from "react-scroll";
 
+// Navigation Links Data
+const navLinks = [
+  { label: "About", to: "about" },
+  { label: "Tech Stack", to: "tech-stack" },
+  { label: "Projects", to: "projects" },
+  { label: "Experience", to: "experience" },
+];
+
 function Navbar() {
   return (
     <nav className="nav">
       <div className="nav__links">
-        <Link
-          to="about"
-          smooth={true}
-          duration={500}
-          offset={-96.5}
-          className="nav__link"
-        >
-          About
-        </Link>
-        <Link
-          to="tech-stack"
-          smooth={true}
-          duration={500}
-          offset={-96.5}
-          className="nav__link"
-        >
-          Tech Stack
-        </Link>
-        <Link
-          to="projects"
-          smooth={true}
-          duration={500}
-          offset={-96.5}
-          className="nav__link"
-        >
-          Projects
-        </Link>
-        <Link
-          to="experience"
-          smooth={true}
-          duration={500}
-          offset={-96.5}
-          className="nav__link"
-        >
-          Experience
-        </Link>
+        {navLinks.map((link, index) => (
+          <Link
+            key={index}
+            to={link.to}
+            smooth={true}
+            duration={500}
+            offset={-96.5}
+            className="nav__link"
+          >
+            {link.label}
+          </Link>
+        ))}
       </div>
     </nav>
   );
